03409ee8eae5d75f75d1837cf5974edd7fc61d60
CompSciDev <CompScienceDev@gmail.com>
2022-05-03 00:06:51 +0200

    update api test to return env var

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,11 @@
 export default function handler(req, res) {
-  res.status(200).json({ name: "Deployed!" });
+  const secret = process.env.SUPER_SECRET_KEY;
+
+  if (!secret) {
+    return res
+      .status(200)
+      .json({ name: "Deployed, but the env variable is missing!" });
+  }
+
+  return res.status(200).json({ name: `Deployed! ${secret}` });
 }
